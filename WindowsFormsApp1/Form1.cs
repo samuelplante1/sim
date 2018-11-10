276d5144c1c864585a43fd89c28cda2a8abce8fc
1729304 <1729304@LLBD0603INFO20>
2018-11-10 15:58:23 -0500

    asdf

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Personne m_objPersonne;
+        private Participant m_objParticipant;
+        private Cegep m_objCegep;
+        private List<Participant> m_listParticipant = new List<Participant>();
+        public Form1()
+        {
+            InitializeComponent();
+            PopulerChampsCegeps();
+        }
+        List<Cegep> listCegep;
+
+
+        private void PopulerChampsCegeps()
+        {
+            sql msql = new sql("SELECT * FROM Cegep ORDER BY 3", "A18_Sim_Eq07");
+            SqlDataReader read = msql.execute();
+            while (read.Read())
+            {
+                cmbCegep.Items.Add(read["nomCegep"]);
+            }
+            read.Close();
+        }
+
+        private void btnInscrire_Click(object sender, EventArgs e)
+        {
+            m_objPersonne = new Personne(txtNom.Text, txtPrenom.Text, txtCourriel.Text);
+            if (cmbExistant.SelectedIndex != -1)
+            {
+                foreach (Participant participant in m_listParticipant)
+                {
+                    if (participant.Nom == txtGroupe.Text)
+                        participant.Ajouter(m_objPersonne);
+                }
+            } else
+            {
+                m_objParticipant = new Participant(txtGroupe.Text);
+                m_listParticipant.Add(m_objParticipant);
+                m_objParticipant.Ajouter(m_objPersonne);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/sql.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class sql
+    {
+        SqlConnection sqlcon;
+        SqlCommand command;
+        public sql(string commande, string use)
+        {
+            sqlcon = new SqlConnection("Data Source=ed4sql2.laboratoire.collegeem.qc.ca; Initial Catalog=" + use + "; User ID=Sim07_User; Pwd=Sim07_=2=3User:2018");
+            sqlcon.Open();
+            command = sqlcon.CreateCommand();
+            command.CommandText = commande;
+        }
+        public SqlDataReader execute() {
+            return command.ExecuteReader();
+        }
+        public void close() {
+            sqlcon.Close();
+        }
+    }
+}
