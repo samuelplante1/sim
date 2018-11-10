f074a1ce8c1532cebd4e918f42aead7fa6905553
1481000 <1481000@LLBD0603INFO21>
2018-11-10 17:18:43 -0500

    Insert
    insertion

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -14,12 +14,15 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         private Personne m_objPersonne;
+        private List<Personne> m_listPersonnes;
         private Participant m_objParticipant;
         private Cegep m_objCegep;
         private List<Participant> m_listParticipant = new List<Participant>();
         public Form1()
         {
             InitializeComponent();
+
+            m_listPersonnes = new List<Personne>();
             PopulerChampsCegeps();
             PopulerChampsGroupes();
             PopulerChampsCategories();
@@ -62,21 +65,46 @@ namespace WindowsFormsApp1
             read.Close();
         }
 
+        //private void ChargerPersonnes()
+        //{
+        //    sql msql = new sql("SELECT * FROM Personne ORDER BY 2, 3", "A18_Sim_Eq07");
+        //    SqlDataReader read = msql.execute();
+        //    while (read.Read())
+        //    {
+        //        m_listPersonnes.Add(new Personne(read["nomCegep"]));
+        //    }
+        //    read.Close();
+        //}
+
         private void btnInscrire_Click(object sender, EventArgs e)
         {
-            m_objPersonne = new Personne(txtNom.Text, txtPrenom.Text, txtCourriel.Text);
-            if (cmbGroupesExistants.SelectedIndex != -1)
+            if(txtNom.Text != "" && txtPrenom.Text != "" && txtCourriel.Text != "" && cmbCategorie.SelectedIndex != -1 && cmbCegep.SelectedIndex != -1)
             {
-                foreach (Participant participant in m_listParticipant)
+                sql insertionPersonneSQL = new sql("INSERT INTO [dbo].[Personne] VALUES ('" +  txtPrenom.Text + "', '" + txtNom.Text +  "', '" + txtCourriel.Text + "', " + "123123" + ")", "A18_Sim_Eq07");
+                insertionPersonneSQL.executeNonQuery();
+
+                sql insertioInscriptionnSQL = new sql("INSERT INTO [dbo].[Inscription] VALUES ()", "A18_Sim_Eq07");
+                insertioInscriptionnSQL.executeNonQuery();
+
+
+                // Ajouter à la liste locale
+                m_objPersonne = new Personne(txtNom.Text, txtPrenom.Text, txtCourriel.Text);
+                if (cmbGroupesExistants.SelectedIndex != -1)
                 {
-                    if (participant.Nom == txtGroupe.Text)
-                        participant.Ajouter(m_objPersonne);
+                    foreach (Participant participant in m_listParticipant)
+                    {
+                        if (participant.Nom == txtGroupe.Text)
+                        {
+                            participant.Ajouter(m_objPersonne);
+                        }
+                    }
                 }
-            } else
-            {
-                m_objParticipant = new Participant(txtGroupe.Text);
-                m_listParticipant.Add(m_objParticipant);
-                m_objParticipant.Ajouter(m_objPersonne);
+                else
+                {
+                    m_objParticipant = new Participant(txtGroupe.Text);
+                    m_listParticipant.Add(m_objParticipant);
+                    m_objParticipant.Ajouter(m_objPersonne);
+                }
             }
         }
 
@@ -98,5 +126,6 @@ namespace WindowsFormsApp1
                 txtGroupe.Enabled = true;
             }
         }
+        //private void VerifierIns
     }
 }
