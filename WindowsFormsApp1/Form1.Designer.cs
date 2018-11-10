c9f118a628860a1c4dfc8e9054016a038dd31732
1729304 <1729304@LLBD0603INFO20>
2018-11-10 13:08:47 -0500

    asdf

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,6 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 129);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+=======
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -37,13 +50,18 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(145, 20);
             this.textBox1.TabIndex = 0;
+>>>>>>> 95c34dd00972f9263cf6f7c751f8b2cee1cf7cca
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+<<<<<<< HEAD
+            this.Controls.Add(this.button1);
+=======
             this.Controls.Add(this.textBox1);
+>>>>>>> 95c34dd00972f9263cf6f7c751f8b2cee1cf7cca
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +71,11 @@
 
         #endregion
 
+<<<<<<< HEAD
+        private System.Windows.Forms.Button button1;
+=======
         private System.Windows.Forms.TextBox textBox1;
+>>>>>>> 95c34dd00972f9263cf6f7c751f8b2cee1cf7cca
     }
 }
 
