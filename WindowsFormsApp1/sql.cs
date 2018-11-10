c692e7efa2cdc8afc21da8151e2c48fb8895ec03
1729304 <1729304@LLBD0603INFO20>
2018-11-10 14:39:33 -0500

    asdf

---
+++ WindowsFormsApp1/sql.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class sql
+    {
+        public SqlDataReader SQL(string commande, string use) {
+            SqlConnection sqlcon = connectToDB(use);
+            SqlCommand command = sqlcon.CreateCommand();
+            command.CommandText = commande;
+            return command.ExecuteReader();
+        }
+
+        public SqlConnection connectToDB(string use)
+        {
+            SqlConnection sqlcon = new SqlConnection("Data Source = ed4sql2; Initial Catalog = " + use + "; Integrated Security = True");
+            sqlcon.Open();
+            return sqlcon;
+        }
+    }
+}
