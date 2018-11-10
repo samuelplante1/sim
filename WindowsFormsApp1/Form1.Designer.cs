752fdf28dc2bc2ff248a2fe320193577b76e972f
1729304 <1729304@LLBD0603INFO20>
2018-11-10 14:08:58 -0500

    asdf

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,36 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.cmbCegep = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbExistant = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cmbCategorie = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(331, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Inscription des Participants";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(77, 124);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 1;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(183, 124);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 2;
+            // 
+            // cmbCegep
+            // 
+            this.cmbCegep.FormattingEnabled = true;
+            this.cmbCegep.Location = new System.Drawing.Point(586, 123);
+            this.cmbCegep.Name = "cmbCegep";
+            this.cmbCegep.Size = new System.Drawing.Size(125, 21);
+            this.cmbCegep.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(583, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cegep";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(585, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(125, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(582, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Date de l\'événement";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nom";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Prenom";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(297, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(250, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Si vous faites partie d\'un groupe, indiquer le numero";
+            // 
+            // cmbExistant
+            // 
+            this.cmbExistant.FormattingEnabled = true;
+            this.cmbExistant.Location = new System.Drawing.Point(300, 124);
+            this.cmbExistant.Name = "cmbExistant";
+            this.cmbExistant.Size = new System.Drawing.Size(247, 21);
+            this.cmbExistant.TabIndex = 10;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(567, 52);
+            this.button1.Location = new System.Drawing.Point(585, 252);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(209, 129);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 40);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "S\'inscrire";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // cmbCategorie
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(319, 95);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.cmbCategorie.FormattingEnabled = true;
+            this.cmbCategorie.Location = new System.Drawing.Point(300, 172);
+            this.cmbCategorie.Name = "cmbCategorie";
+            this.cmbCategorie.Size = new System.Drawing.Size(247, 21);
+            this.cmbCategorie.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(297, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Catégorie";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(816, 323);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbCategorie);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbExistant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCegep);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,14 +188,22 @@
 
         }
 
-        #endregion
-
-<<<<<<< HEAD
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-=======
         private System.Windows.Forms.TextBox textBox1;
->>>>>>> 95c34dd00972f9263cf6f7c751f8b2cee1cf7cca
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.ComboBox cmbCegep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbExistant;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbCategorie;
+        private System.Windows.Forms.Label label6;
+
+        #endregion
     }
 }
 
